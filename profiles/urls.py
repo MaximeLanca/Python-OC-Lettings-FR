338b481e7ee542e433fbf9f1c06cba0a1f61e044
MaximeLanca <lanca.maxime.ber@gmail.com>
2026-06-02 12:50:34 +0200

    refactor: change html file and function name

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'profile'
 
 urlpatterns = [
-    path('', views.profiles_index, name='profiles_index'),
+    path('', views.index, name='index'),
     path('<str:username>/', views.profile, name="profile"),
 ]
